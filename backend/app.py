18998e6c1b2b20437db0713e6c894101c4a761fe
Choenden Kyirong <ckyirong@sfu.ca>
2021-09-23 20:20:56 -0700

    Initial Post Request (#4)
    * adding backend initial files
    
    * adding heroku deployment files
    
    * changing buildfiles and locatioins
    
    * changed welcome message
    
    * initial post request

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return "Hello World! Welcome to the Blackfoot Language Application."
+
+
+@app.route('/audio_data', methods=['POST'])
+@cross_origin()
+def audio_data():
+    if request.method == 'POST':
+        request_data = request.get_json()
+        english_term = request_data['english_term']
+        # audio_file = request.files['audio']
+
+        return jsonify({'response': 'audio file recieved.', 'english_term': english_term})
+
+
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
