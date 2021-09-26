e177738ce43692fed4e9febe07fb8788767fe73d
Brian Gershon <briangershon@users.noreply.github.com>
2021-09-26 10:10:50 -0700

    Add project and team information to About page.

--- frontend/src/About.jsx
+++ frontend/src/About.jsx
@@ -8,6 +8,8 @@ function About() {
         application that allows an English speaker to explore the Blackfoot
         language, and vice versa.
       </p>
+      <p>This was developed as a prototype as part of the Vancouver DataJam 2021.</p>
+      <p>More info about the project and the team are in our <a href="https://github.com/BryceHaley/team7-blackfoot-chat-app" target="_blank">Github Repo</a>.</p>
     </div>
   );
 }
